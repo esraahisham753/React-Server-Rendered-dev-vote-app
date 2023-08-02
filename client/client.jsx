e30ce0ce5fbde3b5d328ec89c405d4d7bd49de8b
Esraa Abduallah <esraahisham753@gmail.com>
2023-08-02 17:00:55 +0000

    Rehydrate the app component

--- client/client.jsx
+++ client/client.jsx
@@ -3,4 +3,34 @@ import ReactDOM from "react-dom";
 
 import { App } from "./app";
 
-ReactDOM.render(<App />, document.querySelector("#container"));
+let state = undefined;
+
+fetch("https://5xwlhr-7777.csb.app/data")
+  .then((data) => data.json())
+  .then((json) => {
+    state = json;
+    console.log("state changed");
+    render();
+  });
+
+//ReactDOM.render(<App />, document.querySelector("#container"));
+
+function handleChangeVote(answerId, increment) {
+  state.answers = state.answers.map((answer) => {
+    if (answer.answerId != answerId) {
+      return answer;
+    } else {
+      return { ...answer, upvotes: answer.upvotes + increment };
+    }
+  });
+  render();
+}
+
+function render() {
+  ReactDOM.hydrate(
+    <App {...state} handleChangeVote={handleChangeVote} />,
+    document.querySelector("#container")
+  );
+}
+
+//render();
